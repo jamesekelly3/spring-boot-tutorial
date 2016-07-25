fcc47d429a2fdc3a521defadaca1fdf6cd216159
James Kelly <jkelly@nabancard.com>
2016-07-25 16:45:57 -0400

    Multiple pages with tiles and controller

--- src/main/java/com/jekh/App.java
+++ src/main/java/com/jekh/App.java
@@ -1,13 +1,14 @@
 package com.jekh;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
-@Controller
-@EnableAutoConfiguration
+
+@SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
@@ -15,8 +16,31 @@ public class App {
 
 	}
 
-	@RequestMapping("/")
-	String home() {
-		return "home";
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(App.class);
+	}*/
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer(){
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
+		
+		String[] defs = {"/WEB-INF/tiles.xml"};
+		
+		tilesConfigurer.setDefinitions(defs);
+		
+		return tilesConfigurer; 
+	}
+	
+	
+	
+	@Bean
+	public UrlBasedViewResolver tilesViewResolver(){
+		
+		UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver(); 
+		
+		tilesViewResolver.setViewClass(TilesView.class);
+		
+		return tilesViewResolver; 
 	}
 }
